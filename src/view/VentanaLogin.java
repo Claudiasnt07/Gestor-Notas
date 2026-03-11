0fb91aed0aef6cb746556747e5f9a4b654b953a3
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:43:35 +0000

    Cree los botones Login y Registrar

--- src/view/VentanaLogin.java
+++ src/view/VentanaLogin.java
@@ -7,4 +7,7 @@ public class VentanaLogin JFrame {
     public JTextField usuario = new JTextField();
     public JPasswordField password = new JPasswordField();
 
+    public JButton botonLogin = new JButton("Login");
+    public JButton botonRegistro = new JButton("Registrar");
+
 }
