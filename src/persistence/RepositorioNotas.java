f92cf1a0f7878ebadaa246f6b4066413a402f9e8
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:21:58 +0000

    Cree el metodo cargarNotas para que podamos leer las notas de los usuarios

---
+++ src/persistence/RepositorioNotas.java
@@ -0,0 +1,26 @@
+package src.persistence;
+
+import model.Nota;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.utils.*;
+
+public class RepositorioNotas {
+    
+    public List<Nota> cargarNotas(String usuario) {
+        List<Nota> notas = new ArrayList<>();
+        String archivo = "notas_" + usuario + ".txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split("\\|");
+                notas.add(new Nota(partes[0], partes[1]));
+            }
+        } catch (Exception e) {}
+
+        return notas;
+    }
+    
+}
