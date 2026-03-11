3a106747bc31d87f2c8a5a3af12e15bf56799b31
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:27:45 +0000

    Cree el atributo repo de RepositorioUsuarios

---
+++ src/service/ServicioUsuarios.java
@@ -0,0 +1,11 @@
+package src.service;
+
+import persistence.RepositorioUsuarios;
+import src.persistence.RepositorioNotas;
+import utils.UtilHash;
+import java.util.Map;
+
+public class ServicioUsuarios {
+    
+    private RepositorioUsuarios repo = new RepositorioUsuarios();
+}
