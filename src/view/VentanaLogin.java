95750bbd632cbd0391d8267b0aa436bde1e50f12
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:44:52 +0000

    Cree etiquetas y componentes para la ventana

--- src/view/VentanaLogin.java
+++ src/view/VentanaLogin.java
@@ -10,4 +10,26 @@ public class VentanaLogin JFrame {
     public JButton botonLogin = new JButton("Login");
     public JButton botonRegistro = new JButton("Registrar");
 
+    public VentanaLogin() {
+        setTitle("Login");
+        setSize(300, 200);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        usuario.setBounds(80, 20, 150, 25);
+        password.setBounds(80, 60, 150, 25);
+
+        botonLogin.setBounds(30, 110, 100, 30);
+        botonRegistro.setBounds(150, 110, 100, 30);
+
+        add(new JLabel("Usuario")).setBounds(20, 20, 80, 25);
+        add(usuario);
+
+        add(new JLabel("Password")).setBounds(20, 60, 80, 25);
+        add(password);
+
+        add(botonLogin);
+        add(botonRegistro);
+
+        setVisible(true);
 }
