aca4feaff453758c4ac2d0f4f8b4bcab6c465d49
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:36:46 +0000

    Cree el getter getNotas

--- src/service/ServicioNotas.java
+++ src/service/ServicioNotas.java
@@ -10,4 +10,12 @@ public class ServicioNotas {
     private RepositorioNotas repo = new RepositorioNotas();
     private String usuario;
 
+    public ServicioNotas(String usuario) {
+        this.usuario = usuario;
+        notas = repo.cargarNotas(usuario);
+    }
+
+    public List<Nota> getNotas() {
+        return notas;
+    }
 }
