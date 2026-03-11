b19391952120baa4baf38bc9e2c60188ac8d548c
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 20:53:51 +0000

    Cree el constructor nota

--- src/model/Nota.java
+++ src/model/Nota.java
@@ -4,4 +4,9 @@ public class Nota {
     
     private String titulo;
     private String contenido;
+
+    public Nota(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
 }
