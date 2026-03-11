5b6a676470638c48e5c6a25dd5b4713744e672e2
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:59:00 +0000

    Cree los metodos login y registrar

--- src/controller/GestorAutenticacion.java
+++ src/controller/GestorAutenticacion.java
@@ -1,6 +1,10 @@
 package src.controller;
 
 import src.view.VentanaLogin;
+import src.view.VentanaNotas;
+
+import javax.swing.JOptionPane;
+
 import src.service.ServicioUsuarios;
 
 public class GestorAutenticacion {
@@ -13,6 +17,33 @@ public class GestorAutenticacion {
 
         vista.botonLogin.addActionListener(e -> login());
         vista.botonRegistro.addActionListener(e -> registrar());
+    }
+
+    private void login() {
+        String u = vista.usuario.getText();
+        String p = new String(vista.password.getPassword());
+
+        if (servicio.login(u, p)) {
+            JOptionPane.showMessageDialog(vista, "Login correcto");
+
+            vista.dispose();
+
+            new ControladorNotas(new VentanaNotas(), u);
+
+        } else {
+            JOptionPane.showMessageDialog(vista, "Error de login");
+        }
+    }
+
+    private void registrar() {
+        String u = vista.usuario.getText();
+        String p = new String(vista.password.getPassword());
+
+        if (servicio.registrar(u, p)) {
+            JOptionPane.showMessageDialog(vista, "Usuario registrado");
+        } else {
+            JOptionPane.showMessageDialog(vista, "Usuario ya existe");
+        }
     }
 }
 
