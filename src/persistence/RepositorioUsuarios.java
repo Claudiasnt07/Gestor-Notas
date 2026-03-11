50add0a9d0757b892907c6a694147f390074d38a
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:19:41 +0000

    Cree el metodo cargarUsuarios

--- src/persistence/RepositorioUsuarios.java
+++ src/persistence/RepositorioUsuarios.java
@@ -1,7 +1,11 @@
 package src.persistence;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RepositorioUsuarios {
     
@@ -13,5 +17,20 @@ public class RepositorioUsuarios {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    public Map<String, String> cargarUsuarios() {
+        Map<String, String> usuarios = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ARCHIVO))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(":");
+                usuarios.put(partes[0], partes[1]);
+            }
+        } catch (Exception e) {
+        }
+
+        return usuarios;
     }
 }
