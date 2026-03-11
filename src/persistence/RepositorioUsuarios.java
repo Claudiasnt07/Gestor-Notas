5dd8f9b2f711c01b9d303e4d5ba3f3bfaeda6097
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:18:33 +0000

    Cree el metodo guardarUsuario

--- src/persistence/RepositorioUsuarios.java
+++ src/persistence/RepositorioUsuarios.java
@@ -1,6 +1,17 @@
 package src.persistence;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class RepositorioUsuarios {
     
     private static final String ARCHIVO = "usuarios.txt";
+
+    public void guardarUsuario(String usuario, String hash) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(ARCHIVO, true))) {
+            pw.println(usuario + ":" + hash);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
