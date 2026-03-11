83e4937aca2f03aa60c9a8f6e1d21086e9a75dad
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:53:03 +0000

    Cree los atributos vista y servicioUsuarios

--- src/controller/GestorAutenticacion.java
+++ src/controller/GestorAutenticacion.java
@@ -1,5 +1,12 @@
 package src.controller;
 
+import src.view.VentanaLogin;
+import src.service.ServicioUsuarios;
+
 public class GestorAutenticacion {
     
+    private VentanaLogin view;
+    private src.service.ServicioUsuarios service = new src.service.ServicioUsuarios();
+
 }
+
--- src/view/VentanaLogin.java
+++ src/view/VentanaLogin.java
@@ -32,4 +32,5 @@ public class VentanaLogin JFrame {
         add(botonRegistro);
 
         setVisible(true);
+    }
 }
