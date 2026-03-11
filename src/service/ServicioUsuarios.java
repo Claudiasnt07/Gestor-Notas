59b91cf78e39ccef754ae66b28c39ed91eb09bc4
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:28:43 +0000

    Cree el metodo registrar para registrar al usuario con hash de password

--- src/service/ServicioUsuarios.java
+++ src/service/ServicioUsuarios.java
@@ -8,4 +8,18 @@ import java.util.Map;
 public class ServicioUsuarios {
     
     private RepositorioUsuarios repo = new RepositorioUsuarios();
+
+    public boolean registrar(String usuario, String password) {
+        Map<String, String> usuarios = repo.cargarUsuarios();
+
+        if (usuarios.containsKey(usuario)) {
+            return false;
+        }
+
+        String hash = UtilHash.generarHash(password);
+
+        repo.guardarUsuario(usuario, hash);
+
+        return true;
+    }
 }
