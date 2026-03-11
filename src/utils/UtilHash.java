84deec474fe813db3a92ce26880ffc28c33a3273
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:05:00 +0000

    Creamos el StringBuilder para poder crear el hash

--- src/utils/UtilHash.java
+++ src/utils/UtilHash.java
@@ -1,10 +1,15 @@
 package src.utils;
 
+import java.security.MessageDigest;
+
 public class UtilHash {
  
     public static String generarHash(String texto) {
         try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(texto.getBytes());
 
+            String resultado = "";
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
