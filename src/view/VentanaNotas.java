0f70bd7f1dbf8d3f5a0bbedba636966ac62f605c
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:47:37 +0000

    Cree botones crear, editar y eliminar

--- src/view/VentanaNotas.java
+++ src/view/VentanaNotas.java
@@ -6,4 +6,11 @@ public class VentanaNotas extends JFrame {
     
     public DefaultListModel<String> modeloLista = new DefaultListModel<>();
     public JList<String> lista = new JList<>(modeloLista);
+
+    public JTextField titulo = new JTextField();
+    public JTextArea contenido = new JTextArea();
+
+    public JButton crear = new JButton("Crear");
+    public JButton editar = new JButton("Editar");
+    public JButton eliminar = new JButton("Eliminar");
 }
