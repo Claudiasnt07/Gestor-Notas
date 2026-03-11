43dcfed39c2f71e38b3e4c56077e543c935bb474
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-11 21:01:18 +0000

    Cree el manejo d eexceptiones para generarHash

--- src/utils/UtilHash.java
+++ src/utils/UtilHash.java
@@ -3,6 +3,10 @@ package src.utils;
 public class UtilHash {
  
     public static String generarHash(String texto) {
-        
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
